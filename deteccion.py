1bb9aec968378084bd726ac8efd917ca22b5fa50
charmts1234 <50456416+charmts1234@users.noreply.github.com>
2021-06-09 16:17:46 -0400

    mi primer commit

---
+++ deteccion.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import imutils
+cap=cv2.VideoCapture(0)
+azulBajo = np.array([100,100,20],np.uint8)
+azulAlto = np.array([125,255,255],np.uint8)
+while True:
+	ret,frame=cap.read()
+	if ret==True:
+		framHSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+		mask = cv2.inRange(frameHSV,azulBajo,azulAlto)
+		cv2.imshow('maskAzul', mask)
+		cv2.imshow('frame', frame)
+		if cv2.waitKey(1) & 0xFF == ord('s'):
+			break
+cap.release()
+cv2.destroyAllWindows()
